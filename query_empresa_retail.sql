23e731e66ce7eb299ec6c2f2cc9564129900ea74
Juan Chamizo <chamizojuan834@gmail.com>
2026-09-12 16:34:10 -0500

    Se agrego la tabla de campaña, cliente y interacion

--- query_empresa_retail.sql
+++ query_empresa_retail.sql
@@ -1,6 +1,1 @@
-/*Tabla canal*/
-insert into canal(can_nombre,can_tipo)
-values ('Facebook','Buscador');
-
-select can_id_canal, can_nombre, can_tipo
-from canal;
+SELECT * FROM `empresa-retail-db`.interaccion;
